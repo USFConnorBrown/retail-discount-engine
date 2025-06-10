728fbfa919e004ea7c0acf5067808d5372bd3c5c
Connor Brown <connorbrown1@usf.edu>
2025-06-09 20:30:29 -0400

    Created step 2 for my coding challenge 04

---
+++ cc_4.js
@@ -0,0 +1,8 @@
+//Step 2: Create an array of objects reprresenting 5 products
+const products = [
+    { name: "Laptop:", category: "electronics", price: 999.99, inventory: 5},
+    { name: "Sweatshirt:", category: "apparel", price: 49.99, inventory: 10},
+    { name: "Eggs:", category: "groceries", price: 9.99, inventory: 12},
+    { name: "Soap:", category: "household", price: 4.99, inventory: 8},
+    { name: "Blender:", category: "Home Appliances", price: 49.99, inventory: 12}
+];
